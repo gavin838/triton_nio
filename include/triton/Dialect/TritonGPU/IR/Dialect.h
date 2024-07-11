0046baafec515f90a7136a83b330190dee8b1bed
Alexander Efimov <efimov.alexander@gmail.com>
2024-07-11 21:44:51 +0200

    support dot 3d in FMA

--- include/triton/Dialect/TritonGPU/IR/Dialect.h
+++ include/triton/Dialect/TritonGPU/IR/Dialect.h
@@ -131,6 +131,22 @@ void dumpHWLayout(RankedTensorType tensorType);
 // Return a string representation of the layout of the tensor.
 std::string getLayoutStr(RankedTensorType tensorType, bool useHWPointOfView);
 
+template <template <typename> typename Vec, typename T>
+llvm::SmallVector<T> expandMatrixShapeWithBatch(Vec<T> s) {
+  llvm::SmallVector<T> expanded(3 - s.size(), 1);
+  expanded.append(s.begin(), s.end());
+  return expanded;
+}
+
+template <template <typename> typename Vec, typename T>
+llvm::SmallVector<T> expandMatrixOrderWithBatch(Vec<T> o) {
+  int oldRank = o.size();
+  llvm::SmallVector<T> expanded(3, 0);
+  for (int i = 0; i < oldRank; ++i)
+    expanded[i] += o[i] + 3 - oldRank;
+  return expanded;
+}
+
 } // namespace gpu
 } // namespace triton
 } // namespace mlir
