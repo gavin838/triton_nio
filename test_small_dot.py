5450a69da33d422434555ae79042b56bc4e87cd4
Alexander Efimov <efimov.alexander@gmail.com>
2024-08-23 14:55:58 +0000

    improve small test, fix optimization sequence in compiler.py

--- test_small_dot.py
+++ test_small_dot.py
@@ -4,6 +4,8 @@ import triton
 import triton.language as tl
 import pytest
 
+device = "cuda"
+
 
 @triton.jit
 def _triton_dot_kernel(
@@ -22,16 +24,21 @@ def _triton_dot_kernel(
     BLOCK_M: tl.constexpr,
     BLOCK_N: tl.constexpr,
     BLOCK_K: tl.constexpr,
+    K: tl.constexpr,
 ):
     off_m = tl.arange(0, BLOCK_M)
     off_n = tl.arange(0, BLOCK_N)
     off_k = tl.arange(0, BLOCK_K)
     As = A + off_m[:, None] * stride_am + off_k[None, :] * stride_ak
     Bs = B + off_k[:, None] * stride_bk + off_n[None, :] * stride_bn
+    c = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.int32)
+    for k in range(0, tl.cdiv(K, BLOCK_K)):
+        a = tl.load(As)
+        b = tl.load(Bs)
+        c += tl.dot(a, b)
+        As += stride_ak * BLOCK_K
+        Bs += stride_bk * BLOCK_K
     Cs = C + off_m[:, None] * stride_cm + off_n[None, :] * stride_cn
-    a = tl.load(As)
-    b = tl.load(Bs)
-    c = tl.dot(a, b)
     tl.store(Cs, c)
 
 
@@ -42,25 +49,29 @@ def _triton_dot3d_kernel(
         # Matrix dimensions
         stride_am, stride_ak, stride_bk, stride_bn, stride_cm, stride_cn,
         # Meta-parameters
-        BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr, BLOCK_K: tl.constexpr, K_SPLIT: tl.constexpr):
+        BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr, BLOCK_K: tl.constexpr, K: tl.constexpr, K_SPLIT: tl.constexpr):
     off_m = tl.arange(0, BLOCK_M)
     off_n = tl.arange(0, BLOCK_N)
     off_k = tl.arange(0, BLOCK_K)
     As = A + off_m[:, None] * stride_am + off_k[None, :] * stride_ak
     Bs = B + off_k[:, None] * stride_bk + off_n[None, :] * stride_bn
+    for k in range(0, tl.cdiv(K, BLOCK_K)):
+        a = tl.load(As)
+        a = tl.reshape(a, (BLOCK_M, K_SPLIT, BLOCK_K // K_SPLIT)).trans(1, 0, 2)
+        b = tl.load(Bs)
+        b = tl.reshape(b, (K_SPLIT, BLOCK_K // K_SPLIT, BLOCK_N))
+        c = tl.dot(a, b)
+        c = tl.sum(c, axis=0)
+        As += stride_ak * BLOCK_K
+        Bs += stride_bk * BLOCK_K
     Cs = C + off_m[:, None] * stride_cm + off_n[None, :] * stride_cn
-    a = tl.load(As)
-    a = tl.reshape(a, (BLOCK_M, K_SPLIT, BLOCK_K // K_SPLIT)).trans(1, 0, 2)
-    b = tl.load(Bs)
-    b = tl.reshape(b, (K_SPLIT, BLOCK_K // K_SPLIT, BLOCK_N))
-    c = tl.dot(a, b)
-    c = tl.sum(c, axis=0)
     tl.store(Cs, c)
 
 
 def gemm_forward(out, a, b, dot3d):
     M, K = a.shape
     K, N = b.shape
+    BLOCK_K = 512
 
     kwargs = [
         a,
@@ -74,6 +85,7 @@ def gemm_forward(out, a, b, dot3d):
         out.stride(1),
         M,
         N,
+        BLOCK_K,
         K,
     ]
     if dot3d:
@@ -86,7 +98,7 @@ def gemm_forward(out, a, b, dot3d):
 
 
 def get_shapes():
-    shapes = [(i, 32, 512) for i in (1, 2, 4, 8)]
+    shapes = [(i, 32, 2048) for i in (1, 2, 4, 8)]
     return shapes
 
 
@@ -99,16 +111,16 @@ name_to_tl_types = {
 }
 
 
-def gen_input(M, N, ty_name, needTrans, seed, device='cuda'):
+def gen_input(M, N, ty_name, needTrans, seed, device):
     d_type = name_to_tl_types[ty_name]
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
 
     if ty_name == 'int8':
         if needTrans:
-            raw_data = torch.randint(-20, 20, (N, M), dtype=torch.int8, device='cuda').T
+            raw_data = torch.randint(-10, 10, (N, M), dtype=torch.int8, device=device).T
         else:
-            raw_data = torch.randint(-20, 20, (M, N), dtype=torch.int8, device='cuda')
+            raw_data = torch.randint(-10, 10, (M, N), dtype=torch.int8, device=device)
 
         return raw_data, raw_data.to(torch.half)
 
@@ -117,8 +129,8 @@ def gen_input(M, N, ty_name, needTrans, seed, device='cuda'):
 def test_gemm(m, n, k):
     torch.random.manual_seed(0)
     with torch.no_grad():
-        a, _ = gen_input(m, k, 'int8', False, 1, device='cuda')
-        b, _ = gen_input(k, n, 'int8', True, 2, device='cuda')
+        a, _ = gen_input(m, k, 'int8', False, 1, device=device)
+        b, _ = gen_input(k, n, 'int8', True, 2, device=device)
         #for i in range(m):
         #    for j in range(k):
         #        a[i, j] = 1 if j == 0 else 0
@@ -126,9 +138,12 @@ def test_gemm(m, n, k):
         #    for j in range(k):
         #        b[j, i] = (i) % 127
 
-        out_torch = torch.matmul(a.to(torch.float32), b.to(torch.float32))
-        out_triton = torch.empty([a.shape[0], b.shape[1]], dtype=torch.half, device=a.device)
+        out_torch = torch.matmul(a.to(torch.int32).cpu(), b.to(torch.int32).cpu())
+        out_triton = torch.empty([a.shape[0], b.shape[1]], dtype=torch.int32, device=a.device)
         gemm_forward(out_triton, a, b, dot3d=False)
 
-        diff = ~np.isclose(out_triton.half().cpu().numpy(), out_torch.half().cpu().numpy(), rtol=1e-2)
+        print("triton: ", out_triton.cpu().numpy())
+        print("ref: ", out_torch.numpy())
+
+        diff = ~np.isclose(out_triton.cpu().numpy(), out_torch.numpy(), rtol=1e-2)
         assert diff.sum() < 10, f"m={m}, n={n}, k={k}"
